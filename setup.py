155fb037ec5a4892b7e0de8a5e47086e8e607b35
Ian Connor <iconnor@projectlounge.com>
2024-07-03 05:42:27 +0000

    0.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='localvolts_api',
-    version='0.4',
+    version='0.5',
     author='Ian Connor',
     author_email='ian@blissai.com',
     description='A Python wrapper for the LocalVolts API',
